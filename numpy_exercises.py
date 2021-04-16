657e029522b11838f13421a55bbc6ddb637bafc7
Mariam Naqvi <naqvi.mariam01@gmail.com>
2021-04-16 09:01:36 -0500

    add more exercises

--- numpy_exercises.py
+++ numpy_exercises.py
@@ -1,6 +1,6 @@
-a = np.array([4, 10, 12, 23, -2, -1, 0, 0, 0, -6, 3, -7])
-
 import numpy as np
+
+a = np.array([4, 10, 12, 23, -2, -1, 0, 0, 0, -6, 3, -7])
 
 # 1. How many negative numbers are there? 
 
@@ -44,4 +44,39 @@ z_score =  centered_data/std
 print (z_score)
 
 # 8. Copy the setup and exercise directions from More Numpy Practice into your numpy_exercises.py and add your solutions. 
+
+## Setup 1
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Use python's built in functionality/operators to determine the following:
+# Exercise 1 - Make a variable called sum_of_a to hold the sum of all the numbers in above list
+
+sum_of_a = np.sum(a)
+print (sum_of_a)
+
+# Exercise 2 - Make a variable named min_of_a to hold the minimum of all the numbers in the above list
+
+min_of_a = np.min(a)
+print (min_of_a)
+
+# Exercise 3 - Make a variable named max_of_a to hold the max number of all the numbers in the above list
+
+max_of_a = np.max(a)
+print (max_of_a)
+
+# Exercise 4 - Make a variable named mean_of_a to hold the average of all the numbers in the above list
+
+mean_of_a = np.mean(a)
+print (mean_of_a)
+
+# Exercise 5 - Make a variable named product_of_a to hold the product of multiplying all the numbers in the above list together
+
+product_of_a = np.product(a)
+print (product_of_a)
+
+# Exercise 6 - Make a variable named squares_of_a. It should hold each number in a squared like [1, 4, 9, 16, 25...]
+
+# Exercise 7 - Make a variable named odds_in_a. It should hold only the odd numbers
+
+# Exercise 8 - Make a variable named evens_in_a. It should hold only the evens.
 
